fd5a3fd18859709134a9c9e6b5d2c2bd270fcd8a
Umami-Socks <200410283+Umami-Socks@users.noreply.github.com>
2025-04-20 17:37:05 -0800

    Update inputValidation.h

--- inputValidation.h
+++ inputValidation.h
@@ -156,3 +156,17 @@ string lineOrCancel (string getInputMessage) {
 
     return userInput;
 };
+
+//Functionality taken from https://stackoverflow.com/questions/6486289/how-to-clear-the-console-in-c
+void Clear()
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
